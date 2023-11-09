6498cc7cd624e2faf5507f59441fe11f0686cb01
Marcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-11-09 20:46:53 -0300

    chore: use Zstack

--- Design Fun/Design Fun/ContentView.swift
+++ Design Fun/Design Fun/ContentView.swift
@@ -9,27 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            Text("Hi there!")
-                .bold()
-                .italic()
-                .foregroundStyle(.green)
+        VStack(content: {
+         Spacer()
+            HStack(content: {
+                ZStack(content: {
+                    Text("🎮")
+                        .font(.largeTitle)
+                    Text("Sold out")
+                        .foregroundStyle(.red)
+                        .rotationEffect(.degrees(-15))
+                })
+                Text("🎮")
+                    .font(.largeTitle)
+                Text("🚣‍♀️")
+                    .font(.largeTitle)
+                Text("📱")
+                    .font(.largeTitle)
+            })
             
-            Text("Goodbye")
-                .bold()
-                .foregroundStyle(.blue)
-            
-            Text("Tap me")
-                .font(.largeTitle)
-                .padding()
-                .background(Color.green)
-                .foregroundStyle(.white)
-                .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
-                .grayscale(0.30)
-        }
+        })
+        
     }
 }
 
 #Preview {
     ContentView()
 }
+
+//Text("Tap me")
+//    .font(.largeTitle)
+//    .padding()
+//    .background(Color.green)
+//    .foregroundStyle(.white)
+//    .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+//    .grayscale(0.30)
