df23355ed10d09974269ad073d70a6db3d36b023
Marcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-11-14 00:28:59 -0300

    feat: counter

--- Counter/Counter/ContentView.swift
+++ Counter/Counter/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var count = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(content: {
+            Text("People: \(count)")
+                .font(.largeTitle)
+                .padding()
+            
+            Text(String(count))
+                .font(.largeTitle)
+                .padding()
+        })
+        
+        Button("Add", action: {
+            count += 1
+        })
+        .font(.title)
+        if(count > 0){
+            Button("Remove", action: {
+                if(count > 0){
+                    count -= 1
+                }
+            })
+            .font(.title )
+            .foregroundColor(.red)
         }
-        .padding()
     }
 }
 
