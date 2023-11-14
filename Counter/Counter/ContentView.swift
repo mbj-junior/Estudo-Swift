baf2ab4d20cb99d90f2d1d5690a582edaeb87e1c
Marcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-11-13 23:32:05 -0300

    feat: start counter

---
+++ Counter/Counter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Marcio Botelho Junior on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Counter/Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Marcio Botelho Junior on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
