e42d091ecc6a16e37bfba8f2b73eec1fea80c238
Marcio Botelho Junior <marcio.botelhojunior@gmail.com>
2023-12-20 21:26:31 -0300

    feat: start class and struct

---
+++ MyPlayground.playground/Pages/Class.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+class Dog{
+    var name = ""
+    var age = 0
+    var furColor = ""
+    
+}
+
+var myDog = Dog()
+myDog.age = 7
+myDog.furColor = "Brow"
+myDog.name = "Fido"
+
+var neighborDog = Dog()
+neighborDog.age = 8
+neighborDog.furColor = "White"
+neighborDog.name = "Poodle"
+
+
+
+struct Car {
+    var year: Int
+    var color: String
+    
+}
+
+Car(year: 1992, color: "Red")
